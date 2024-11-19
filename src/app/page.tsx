e6476e7084dc08de3e17449fd9dba0f894fd1ab6
Ali-Ahmed999 <150151136+Ali-Ahmed999@users.noreply.github.com>
2024-11-19 21:11:20 +0500

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import Forms from "@/components/Forms";
+import Grid from "@/components/Grid";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+   <div>
+    <Forms />
+    <Grid />
+    
+   </div>
+  );
+}
